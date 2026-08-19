b856a6ad6a87986d6e0c782afae5dcf232a839e8
celleree <113010938+celleree@users.noreply.github.com>
2026-08-19 12:34:29 -0700

    Add media storage interface

---
+++ lib/media/storage.ts
@@ -0,0 +1,13 @@
+import type { MediaAsset, StoredMediaFile } from '@/lib/media/types';
+
+export class MediaValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'MediaValidationError';
+  }
+}
+
+export interface MediaStorage {
+  saveImage(file: File): Promise<MediaAsset>;
+  readImage(fileName: string): Promise<StoredMediaFile | null>;
+}
