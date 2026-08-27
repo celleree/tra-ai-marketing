3af8a0fd15254e1caa48daa2e08a94aeffb21ade
celleree <113010938+celleree@users.noreply.github.com>
2026-08-27 14:02:57 -0800

    Show real landing page creatives

--- components/landing/momentum-landing.tsx
+++ components/landing/momentum-landing.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { storeLandingCreativeDraft } from '@/lib/creatives/landing-draft';
@@ -12,7 +13,29 @@ import styles from './momentum-landing.module.css';
 
 const COLUMN_COUNT = 6;
 const CARDS_PER_COLUMN = 7;
-const CARD_ITEMS = Array.from({ length: CARDS_PER_COLUMN }, (_, index) => index);
+
+const LANDING_CREATIVES = [
+  '/landing-creatives/we-get-it-and-we-can-help.png',
+  '/landing-creatives/tax-stress-finally-has-a-plan.png',
+  '/landing-creatives/start-your-free-tax-relief-consultation.png',
+  '/landing-creatives/find-peace-of-mind-with-tax-relief.png',
+  '/landing-creatives/business-owners-irs-back-taxes-social-post.png',
+  '/landing-creatives/irs-notice-start-here-notes.png',
+  '/landing-creatives/owe-the-irs-understand-your-options.png',
+  '/landing-creatives/find-relief-from-tax-problems.png',
+  '/landing-creatives/irs-tax-issues-clear-next-steps.png',
+  '/landing-creatives/tax-debt-get-clear-next-steps.png',
+  '/landing-creatives/just-got-a-tax-notice.png',
+  '/landing-creatives/owe-the-irs-5k-50k-500k.png',
+  '/landing-creatives/testimonial-joseph.png',
+  '/landing-creatives/testimonial-mark.png',
+];
+
+function creativeForCard(columnIndex: number, cardIndex: number) {
+  return LANDING_CREATIVES[
+    (columnIndex * CARDS_PER_COLUMN + cardIndex) % LANDING_CREATIVES.length
+  ];
+}
 
 export function MomentumLanding() {
   const router = useRouter();
@@ -69,14 +92,24 @@ export function MomentumLanding() {
             <div className={styles.columnTrack}>
               {[0, 1].map((setIndex) => (
                 <div className={styles.cardSet} key={setIndex}>
-                  {CARD_ITEMS.map((cardIndex) => (
-                    <div
-                      className={styles.placeholder}
-                      key={`${columnIndex}-${setIndex}-${cardIndex}`}
-                    >
-                      <span>TRA</span>
-                    </div>
-                  ))}
+                  {Array.from({ length: CARDS_PER_COLUMN }, (_, cardIndex) => {
+                    const src = creativeForCard(columnIndex, cardIndex);
+                    return (
+                      <div
+                        className={styles.creativeCard}
+                        key={`${columnIndex}-${setIndex}-${cardIndex}`}
+                      >
+                        <Image
+                          src={src}
+                          alt=""
+                          width={1254}
+                          height={1672}
+                          className={styles.creativeImage}
+                          sizes="(max-width: 520px) 42vw, (max-width: 780px) 30vw, 18vw"
+                        />
+                      </div>
+                    );
+                  })}
                 </div>
               ))}
             </div>
